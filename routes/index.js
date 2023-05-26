eb3881add5c30d90da29427a01bb54a858ee3b9e
Anthony Medina <anthony.john.medina@gmail.com>
2023-05-26 19:55:40 -0400

    More work on Dynamic Hero

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router()
 router.get('/', (req, res, next) => {
   res.render('index', {
     title: 'GameNight | The Stress-Free Way to Plan and Play',
-    headerText: 'Picture this:',
+    headerText: 'PICTURE THIS:',
     headerSubText:
       'Chilling with pals, planning a Dungeons and Dragons session. Excitement builds as the date approaches, but scheduling conflicts arise. Fear not, for GameNight is here to save the day! Bid farewell to rescheduling madness with our event management brilliance. Say goodbye to chaos and hello to uninterrupted gaming bliss. Let the games begin!',
-    headerImg: 'header-home.png'
+      headerAction: 'Sign Up'
   })
 })
 
