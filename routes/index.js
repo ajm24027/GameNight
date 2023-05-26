67b8ea00b1d26e15082d0123373dffff9876a13b
Anthony Medina <anthony.john.medina@gmail.com>
2023-05-26 18:19:32 -0400

    Adding more dynamic content

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.get('/', (req, res, next) => {
     title: 'GameNight | The Stress-Free Way to Plan and Play',
     headerText: 'Picture this:',
     headerSubText:
-      'chilling with pals, planning a Dungeons and Dragons session. Excitement builds as the date approaches, but scheduling conflicts arise. Fear not, for GameNight is here to save the day! Bid farewell to rescheduling madness with our event management brilliance. Say goodbye to chaos and hello to uninterrupted gaming bliss. Let the games begin!'
+      'Chilling with pals, planning a Dungeons and Dragons session. Excitement builds as the date approaches, but scheduling conflicts arise. Fear not, for GameNight is here to save the day! Bid farewell to rescheduling madness with our event management brilliance. Say goodbye to chaos and hello to uninterrupted gaming bliss. Let the games begin!',
+    headerImg: 'header-home.png'
   })
 })
 
