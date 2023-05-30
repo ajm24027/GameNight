20430828edd4e176e94cf2c8c97f3ceda5bf47dd
Anthony Medina <anthony.john.medina@gmail.com>
2023-05-30 09:28:03 -0400

    pre-profile feature

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,7 @@ router.get('/logout', function (req, res) {
 
 router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'GameNight | The Stress-Free Way to Plan and Play',
-    headerBack: 'header-home.jpg',
-    headerText: 'FROM LFG TO GG IN NO TIME.',
-    headerSubText:
-      'No more wasted weeks, last-minute reschedules! Embark on epic hijinks, forge legendary friendships. Say goodbye to tedious texts and calls. Get the tools to make sure your party is ready to roll!',
-    headerAction: 'Sign Up'
+    title: 'GameNight | The Stress-Free Way to Plan and Play'
   })
 })
 
